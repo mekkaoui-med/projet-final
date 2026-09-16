ab269ad4030b8722748f2605da750470fde51e0b
mekkaoui mohamed <mekkaouimed20@.com>
2026-09-17 00:00:09 +0100

    projet final : add the normaliserNom function

--- src/progression.js
+++ src/progression.js
@@ -1,3 +1,14 @@
 const { apprenants } = require("./data");
 
+function normaliserNom(nom) {
+  let nomNormalise = nom.trim().toLowerCase();
+  let nomVerification = /^[a-z ]+$/;
+  if (nomVerification.test(nomNormalise)) {
+    return nomNormalise;
+  } else {
+  }
+}
 
+module.exports = {
+  normaliserNom,
+};
